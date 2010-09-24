84d6b65eacb65da8378e0f2a3a7f3deb94a6df1f
Jens Låås <jens@laas.mine.nu>
2010-09-24 10:06:50 +0200

    Revert "try workaround a possible dietlibc bug"
    This reverts commit 4abfb4dcf854f9a743ef8aec481395d293218af8.

--- aff.c
+++ aff.c
@@ -709,17 +709,14 @@ struct queue *queue_new(const char *name, int n, const char *fn)
 int is_netdev(const char *name)
 {
 	char *p;
-	char buf[256];
 
-	strncpy(buf, name, sizeof(buf)-1);
-	buf[sizeof(buf)-1] = 0;
-	
-	if( (p = strchr(buf, '-')) ) {
+	name = strdup(name);
+	if( (p = strchr(name, '-')) ) {
 	  *p = 0;
 	}
-	
+
 	/* this will work with namespaces too */
-	return if_nametoindex(buf);
+	return if_nametoindex(name);
 }
 
 
